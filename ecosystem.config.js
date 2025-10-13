7a56e2bc79bc596cdd0e2c16a6094d88fb511f5b
Mohammed Moudachir BOURAIMA <bouraima224@gmail.com>
2025-10-13 19:16:28 +0000

    Mise à jour de la config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,6 +8,7 @@ module.exports = {
     autorestart: true,
     watch: false,
     max_memory_restart: '1G',
+    exec_mode: 'fork',
     
     // Environnement de production (par défaut)
     env: {
