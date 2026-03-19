2863187c7496559208dfcb99a9589cb062f6b39b
Mohammed Moudachir BOURAIMA <bouraima224@gmail.com>
2026-03-19 16:31:55 +0000

    Ajout d'un bouton retour au niveau de la page des config

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -3,9 +3,12 @@ import React, { useState } from 'react'
 import AdminReportComponent from '../component/admin/reports'
 import AdminCategoriesComponent from '../component/admin/categories'
 import { Report, ReportCategory } from '@/types/reports'
+import { SquareArrowLeft } from 'lucide-react'
+import { useRouter } from 'next/navigation';
 
 
 const AdminPage: React.FC = () => {
+    const router = useRouter();
     const [categories, setCategories] = useState<ReportCategory[]>([]);
     const [reports, setReports] = useState<Report[]>([]);
     const [isLoading, setIsLoading] = useState(true);
@@ -36,7 +39,11 @@ const AdminPage: React.FC = () => {
 
     return (
         <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100">
+
             <div className="container mx-auto px-4 py-8 gap-5">
+                <button onClick={() => router.back()} className={`rounded transition-colors bg-blue-500 text-white`} aria-label="Administration des rapports">
+                    <SquareArrowLeft />
+                </button>
                 <AdminReportComponent categories={categories} reports={reports} isLoading={isLoading} fetchData={fetchData} />
                 <AdminCategoriesComponent categories={categories} fetchCategories={fetchData} isLoading={isLoading} />
             </div>
