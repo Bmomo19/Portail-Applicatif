bec5c1409fd88ca4358b964d7fd82a252062a413
Mohammed Moudachir BOURAIMA <bouraima224@gmail.com>
2026-07-15 17:35:39 +0000

    Fix : Correction de terminologie campagnes <=> communication

--- src/app/campagnes/page.tsx
+++ src/app/campagnes/page.tsx
@@ -57,7 +57,7 @@ export default function CampagnesPage() {
                         <Search className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400" size={20} />
                         <input
                             type="text"
-                            placeholder="Rechercher une campagne..."
+                            placeholder="Rechercher une video..."
                             value={searchQuery}
                             onChange={(e) => setSearchQuery(e.target.value)}
                             className="w-full pl-10 pr-4 py-3 font-semibold text-black border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent outline-none"
